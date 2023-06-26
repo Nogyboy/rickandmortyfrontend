29a44ac630b89ba7565cadc86d4b66ff48d8ec6f
Nogyboy <guamanc9@gmail.com>
2023-06-26 15:18:02 -0500

    Code Prettier

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,15 @@
-import React,{Fragment} from 'react'
+import React, { Fragment } from "react";
 
- 
-const Header = () => { 
+const Header = () => {
   return (
     <>
-    <header className="bg-gray-800 py-6">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold text-white">
-          Rick and Morty API
-        </h1>
-      </div>
-    </header>
+      <header className="bg-gray-800 py-6">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-white">Rick and Morty API</h1>
+        </div>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,67 +1,72 @@
-import React, { useEffect, useState } from "react"; 
+import React, { useEffect, useState } from "react";
 
 import useFetchCharacters from "../hooks/useFetchCharacters";
-import { Combobox } from '@headlessui/react'
-
+import { Combobox } from "@headlessui/react";
 
 const Search = () => {
-    const [render, setRender] = useState(false);
-    const [selectedPerson, setSelectedPerson] = useState<any>(null)
-    const [query, setQuery] = useState<any>(null)  
-    const { characters, error, setPage, page, setName, name, clearName }: any = useFetchCharacters();
- 
+  const [render, setRender] = useState(false);
+  const [selectedPerson, setSelectedPerson] = useState<any>(null);
+  const [query, setQuery] = useState<any>(null);
+  const { characters, error, setPage, page, setName, name, clearName }: any =
+    useFetchCharacters();
 
-    const handleClearSearch = () => {
-        setName("");
-        setPage(1)
-        setQuery(null); 
-        window.location.reload();
+  const handleClearSearch = () => {
+    setName("");
+    setPage(1);
+    setQuery(null);
+    window.location.reload();
+  };
+
+  useEffect(() => {
+    setRender(false);
+    if (
+      query !== null &&
+      query !== undefined &&
+      characters !== undefined &&
+      characters.results &&
+      characters.results.length > 0 &&
+      error === null &&
+      error !== "Error" &&
+      query.length > 0
+    ) {
+      setName(query);
+      setRender(true);
+    } else if (error === "Error" && characters.length === 0) {
+      setName("");
+      setRender(false);
+    } else {
+      setRender(false);
     }
+  }, [query]);
 
-    useEffect(() => {
-        setRender(false)
-        if (query !== null && query !== undefined && characters !== undefined && characters.results && characters.results.length > 0 && error === null && error !== "Error" && query.length > 0) {
-            setName(query);
-            setRender(true);
-        } else if (error === "Error" && characters.length === 0) {
-            setName("");
-            setRender(false);
-        } else {
-            setRender(false);
-        }
-    }, [query]);
-
-    return (
-        <>
-            <div className="relative pt-8 pb-6">
-                <div className="relative">
-                    <Combobox value={selectedPerson} onChange={setSelectedPerson} >
-                        <Combobox.Input
-                            className="w-full px-4 py-2 border-2 border-gray-500 rounded-md focus:outline-none focus:ring focus:ring-gray-500"
-                            placeholder="Buscar por personaje..."
-                            onChange={(event:any) => {
-                                const inputValue = event.target.value;
-                                if (inputValue === null || inputValue === undefined) {
-                                    setQuery(null);
-                                } else {
-                                    setQuery(inputValue);
-                                }
-                            }}
-                        />
-                        <button
-                            onClick={handleClearSearch}
-                            className="absolute right-0 top-0 bg-cyan-500 hover:bg-cyan-400 text-white font-medium h-full w-1/12 rounded-l-full"
-                        >
-                            Borrar
-                        </button>
-                         
-                    </Combobox>
-
-                </div>
-            </div>
-
-        </>
-    )
-}
+  return (
+    <>
+      <div className="relative pt-8 pb-6">
+        <div className="relative">
+          <Combobox value={selectedPerson} onChange={setSelectedPerson}>
+            <Combobox.Input
+              className="w-full px-4 py-2 border-2 border-gray-500 rounded-md focus:outline-none focus:ring focus:ring-gray-500"
+              placeholder="Buscar por personaje..."
+              onChange={(event: any) => {
+                const inputValue = event.target.value;
+                if (inputValue === null || inputValue === undefined) {
+                  setQuery(null);
+                } else {
+                  setQuery(inputValue);
+                }
+              }}
+            />
+            <button
+              onClick={handleClearSearch}
+              className="absolute right-0 top-0 bg-cyan-500 hover:bg-cyan-400 text-white font-medium h-full w-1/12 rounded-l-full"
+            >
+              Borrar
+            </button>
+          </Combobox>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Search;
